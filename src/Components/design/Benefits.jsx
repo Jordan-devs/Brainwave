7f05643701ca3d920de6e0f34acc7dfbe0b46584
Olamide <olamideoduola5@gmail.com>
2025-04-26 15:42:03 +0100

    brainwave

---
+++ src/Components/design/Benefits.jsx
@@ -0,0 +1,5 @@
+export const GradientLight = () => {
+  return (
+    <div className="absolute rounded-full top-0 left-1/4 w-full aspect-square bg-linear-90 from-[#28206C] to-[#28206C]/0 to-70% pointer-events-none" />
+  );
+};
